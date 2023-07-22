7cc2aafaa0fd233f5541d4f66234ffcd7f55f921
JC <39209141+jhc13@users.noreply.github.com>
2023-07-23 00:50:57 +0900

    Add image index label to image list

--- image-tagging-gui/image_list.py
+++ image-tagging-gui/image_list.py
@@ -1,5 +1,6 @@
-from PySide6.QtCore import QSettings, QSize, Qt, Slot
-from PySide6.QtWidgets import QDockWidget, QListView
+from PySide6.QtCore import QModelIndex, QSettings, QSize, Qt, Slot
+from PySide6.QtWidgets import (QDockWidget, QLabel, QListView, QVBoxLayout,
+                               QWidget)
 
 from image_list_model import ImageListModel
 
@@ -8,15 +9,27 @@ class ImageList(QDockWidget):
     def __init__(self, settings: QSettings, image_list_model: ImageListModel):
         super().__init__()
         self.settings = settings
+        self.image_list_model = image_list_model
         # Each `QDockWidget` needs a unique object name for saving its state.
         self.setObjectName('image_list')
         self.setWindowTitle('Images')
         self.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+
         self.list_view = QListView(self)
-        self.list_view.setModel(image_list_model)
+        self.list_view.setModel(self.image_list_model)
         self.list_view.setWordWrap(True)
         self.set_image_width()
-        self.setWidget(self.list_view)
+        self.image_index_label = QLabel()
+
+        # A container widget is required to use a layout with a `QDockWidget`.
+        container = QWidget()
+        layout = QVBoxLayout(container)
+        layout.addWidget(self.list_view)
+        layout.addWidget(self.image_index_label)
+        self.setWidget(container)
+
+        self.list_view.selectionModel().currentChanged.connect(
+            self.update_image_index_label)
 
     @Slot()
     def set_image_width(self):
@@ -24,3 +37,9 @@ class ImageList(QDockWidget):
         # If the actual height of the image is greater than 3 times the width,
         # the image will be scaled down to fit.
         self.list_view.setIconSize(QSize(image_width, image_width * 3))
+
+    @Slot()
+    def update_image_index_label(self, image_index: QModelIndex):
+        image_count = self.image_list_model.rowCount()
+        self.image_index_label.setText(
+            f'Image {image_index.row() + 1} / {image_count}')
