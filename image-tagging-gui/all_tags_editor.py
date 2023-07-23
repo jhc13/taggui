94d33f7ec983b53717614671600057429ac50a51
JC <39209141+jhc13@users.noreply.github.com>
2023-07-24 01:29:43 +0900

    Implement filtering for all tags list

--- image-tagging-gui/all_tags_editor.py
+++ image-tagging-gui/all_tags_editor.py
@@ -1,5 +1,6 @@
 from PySide6.QtCore import QSortFilterProxyModel, Qt
-from PySide6.QtWidgets import QDockWidget, QListView, QVBoxLayout, QWidget
+from PySide6.QtWidgets import (QDockWidget, QLineEdit, QListView, QVBoxLayout,
+                               QWidget)
 
 from tag_counter_model import TagCounterModel
 
@@ -8,6 +9,13 @@ class ProxyTagCounterModel(QSortFilterProxyModel):
     def __init__(self, tag_counter_model: TagCounterModel):
         super().__init__()
         self.setSourceModel(tag_counter_model)
+
+
+class FilterLineEdit(QLineEdit):
+    def __init__(self):
+        super().__init__()
+        self.setPlaceholderText('Search tags')
+        self.setStyleSheet('padding: 8px;')
 
 
 class AllTagsList(QListView):
@@ -26,9 +34,15 @@ class AllTagsEditor(QDockWidget):
         self.setWindowTitle('All Tags')
         self.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
         proxy_tag_counter_model = ProxyTagCounterModel(tag_counter_model)
-        self.all_tags_list = AllTagsList(proxy_tag_counter_model)
+        proxy_tag_counter_model.setFilterRole(Qt.EditRole)
+        filter_line_edit = FilterLineEdit()
+        all_tags_list = AllTagsList(proxy_tag_counter_model)
         # A container widget is required to use a layout with a `QDockWidget`.
         container = QWidget()
         layout = QVBoxLayout(container)
-        layout.addWidget(self.all_tags_list)
+        layout.addWidget(filter_line_edit)
+        layout.addWidget(all_tags_list)
         self.setWidget(container)
+
+        filter_line_edit.textChanged.connect(
+            proxy_tag_counter_model.setFilterFixedString)
