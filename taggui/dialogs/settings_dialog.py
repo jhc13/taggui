db4953a02cac26a0151b5d12daed1ce72785184b
JC <39209141+jhc13@users.noreply.github.com>
2024-01-20 02:51:01 +0900

    Disallow empty tag separator

--- taggui/dialogs/settings_dialog.py
+++ taggui/dialogs/settings_dialog.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import QSettings, Qt
+from PySide6.QtCore import QSettings, Qt, Slot
 from PySide6.QtWidgets import (QDialog, QGridLayout, QLabel, QLineEdit,
                                QSpinBox, QVBoxLayout)
 
@@ -10,9 +10,10 @@ class SettingsDialog(QDialog):
         super().__init__(parent)
         self.settings = settings
         self.setWindowTitle('Settings')
-        self.restart_label = QLabel(
-            'Restart the application to apply new settings.')
-        self.restart_label.setStyleSheet('color: red;')
+        self.restart_warning = 'Restart the application to apply new settings.'
+        self.warning_label = QLabel(self.restart_warning)
+        self.warning_label.setAlignment(Qt.AlignCenter)
+        self.warning_label.setStyleSheet('color: red;')
         layout = QVBoxLayout(self)
         layout.setContentsMargins(20, 20, 20, 20)
         layout.setSpacing(20)
@@ -33,14 +34,19 @@ class SettingsDialog(QDialog):
             self.get_insert_space_after_tag_separator_check_box(), 3, 1,
             Qt.AlignLeft)
         layout.addLayout(grid_layout)
-        # Prevent the grid layout from moving to the center when the restart
+        # Prevent the grid layout from moving to the center when the warning
         # label is hidden.
         layout.addStretch()
-        layout.addWidget(self.restart_label)
-        # Fix the size of the dialog to its size when the restart label is
+        layout.addWidget(self.warning_label)
+        # Fix the size of the dialog to its size when the warning label is
         # shown.
         self.setFixedSize(self.sizeHint())
-        self.restart_label.hide()
+        self.warning_label.hide()
+
+    @Slot()
+    def show_restart_warning(self):
+        self.warning_label.setText(self.restart_warning)
+        self.warning_label.show()
 
     def get_font_size_spin_box(self) -> QSpinBox:
         font_size_spin_box = QSpinBox()
@@ -48,7 +54,7 @@ class SettingsDialog(QDialog):
         font_size_spin_box.setValue(self.settings.value('font_size', type=int))
         font_size_spin_box.valueChanged.connect(
             lambda value: self.settings.setValue('font_size', value))
-        font_size_spin_box.valueChanged.connect(self.restart_label.show)
+        font_size_spin_box.valueChanged.connect(self.show_restart_warning)
         return font_size_spin_box
 
     def get_image_list_image_width_spin_box(self) -> QSpinBox:
@@ -61,16 +67,24 @@ class SettingsDialog(QDialog):
             lambda value: self.settings.setValue('image_list_image_width',
                                                  value))
         image_list_image_width_spin_box.valueChanged.connect(
-            self.restart_label.show)
+            self.show_restart_warning)
         return image_list_image_width_spin_box
+
+    @Slot(str)
+    def handle_tag_separator_change(self, tag_separator: str):
+        if not tag_separator:
+            self.warning_label.setText('The tag separator cannot be empty.')
+            self.warning_label.show()
+            return
+        self.settings.setValue('tag_separator', tag_separator)
+        self.show_restart_warning()
 
     def get_tag_separator_line_edit(self) -> QLineEdit:
         tag_separator_line_edit = QLineEdit()
         tag_separator_line_edit.setText(self.settings.value('tag_separator'))
         tag_separator_line_edit.setMaximumWidth(50)
         tag_separator_line_edit.textChanged.connect(
-            lambda text: self.settings.setValue('tag_separator', text))
-        tag_separator_line_edit.textChanged.connect(self.restart_label.show)
+            self.handle_tag_separator_change)
         return tag_separator_line_edit
 
     def get_insert_space_after_tag_separator_check_box(self) -> BigCheckBox:
@@ -82,5 +96,5 @@ class SettingsDialog(QDialog):
                 'insert_space_after_tag_separator',
                 state == Qt.CheckState.Checked.value))
         insert_space_after_tag_separator_check_box.stateChanged.connect(
-            self.restart_label.show)
+            self.show_restart_warning)
         return insert_space_after_tag_separator_check_box
