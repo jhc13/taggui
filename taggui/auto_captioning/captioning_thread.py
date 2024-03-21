08870c6a828b07c71ba384860f744ef911706c60
JC <39209141+jhc13@users.noreply.github.com>
2024-03-22 00:30:54 +0900

    Use NF4 4-bit quantization type

--- taggui/auto_captioning/captioning_thread.py
+++ taggui/auto_captioning/captioning_thread.py
@@ -164,6 +164,7 @@ class CaptioningThread(QThread):
             if load_in_4_bit:
                 quantization_config = BitsAndBytesConfig(
                     load_in_4bit=True,
+                    bnb_4bit_quant_type='nf4',
                     bnb_4bit_compute_dtype=torch.float16
                 )
                 dtype_argument = {}
