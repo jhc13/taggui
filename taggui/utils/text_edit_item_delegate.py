b27c1f2532cae2cafae78f20ad2c74ae92a78a8c
JC <39209141+jhc13@users.noreply.github.com>
2024-04-01 17:07:10 +0900

    Add left padding to tags in lists

--- taggui/utils/text_edit_item_delegate.py
+++ taggui/utils/text_edit_item_delegate.py
@@ -1,16 +1,24 @@
 from PySide6.QtCore import QEvent, QItemSelectionModel, Qt
-from PySide6.QtWidgets import QPlainTextEdit, QStyledItemDelegate
+from PySide6.QtWidgets import QFrame, QPlainTextEdit, QStyledItemDelegate
 
 
 class TextEditItemDelegate(QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        # Add some left padding.
+        option.rect.adjust(4, 0, 0, 0)
+        super().paint(painter, option, index)
+
     def createEditor(self, parent, option, index):
         editor = QPlainTextEdit(parent)
+        editor.setFrameStyle(QFrame.Shape.NoFrame)
+        editor.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        editor.setStyleSheet('padding-left: 3px;')
         editor.index = index
         return editor
 
     def sizeHint(self, option, index):
         size = super().sizeHint(option, index)
-        size.setHeight(size.height() + 12)
+        size.setHeight(size.height() + 8)
         return size
 
     def eventFilter(self, editor, event: QEvent):
